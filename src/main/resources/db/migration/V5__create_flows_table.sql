12cef0165f838ea6b6e7b47103b811e3066684b2
Giri Dandu <giri.dandu@lookout.com>
2015-03-02 13:17:58 -0500

    Update after code review  - Merge the DB migration scripts, added foreign key for deployments    in the create flows  - Fixes #68

--- src/main/resources/db/migration/V5__create_flows_table.sql
+++ src/main/resources/db/migration/V5__create_flows_table.sql
@@ -13,3 +13,6 @@ CREATE TABLE flows (
 
     PRIMARY KEY (id)
 );
+
+ALTER TABLE deployments ADD COLUMN flowId BIGINT(11);
+ALTER TABLE deployments ADD FOREIGN KEY (flowId) REFERENCES flows(id);
--- src/main/resources/db/migration/V6__deployments_flowid.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE deployments ADD COLUMN flowId BIGINT(11);
-ALTER TABLE deployments ADD FOREIGN KEY (flowId) REFERENCES flows(id);
